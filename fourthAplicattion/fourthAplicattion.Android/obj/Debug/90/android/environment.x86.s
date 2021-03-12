f1c9459a3c073b8b96109c14e898713576c4df4f
jose <jose>
2021-03-12 11:44:54 -0400

    Probando y agregando nuevos codigos al proyecto

--- fourthAplicattion/fourthAplicattion.Android/obj/Debug/90/android/environment.x86.s
+++ fourthAplicattion/fourthAplicattion.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"60404227-8126-437e-9749-6ee9d5576d6a"
+	.asciz	"eb2cfe1e-60a8-4b55-8a69-106dd51e32fa"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
